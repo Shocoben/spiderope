e307f5868e49bbf36b61b69bbdb7b563fff3cc8d
EmmAlvado <emma.alavdo@gmail.com>
2013-05-27 15:44:22 +0200

    update loader

--- bin/setup/gameimages.js
+++ bin/setup/gameimages.js
@@ -8,7 +8,10 @@ define(function () {
     ,["droptherope", "/tuto2.png"]
     ,["cartontall", "/carton1.png"]
     ,["cartonmiddle", "/carton2.png"]
+    ,["flou", "/flouCamera.png"]
+    ,["isart", "/isartBLC.png"]
     ,["player", "/araignee.png"]
+    ,["die", "/araigneeDie.png"]
 	];
 
 	return gameImages;
